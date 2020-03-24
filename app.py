3b75f82eae3e55fddb2a9c0ef87b2d15ac6d5349
Kes Cardoso <kesia.development@gmail.com>
2020-03-24 17:37:04 +0000

    Connect database using flask and mongodb

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 # Environment Variables
@@ -8,18 +8,17 @@ if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-
-# Constants from pymongo
-MONGODB_URI = os.getenv("MONGO_URI")
-DBS_NAME = "task_manager"
+app.config["MONGO_NAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["MONGO_URI"] = 'mongodb+srv://root:r00tUs3r@myfirstcluster-puxzn.mongodb.net/task_manager?retryWrites=true&w=majority'
